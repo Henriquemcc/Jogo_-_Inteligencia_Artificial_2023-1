d638b0b933fa7ab6a5bf70681f2f93870ed4cf02
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-06-08 13:54:23 -0300

    Movendo classe 'Coordenadas' para 'coordenadas.js'.

--- script.js
+++ script.js
@@ -1,48 +1,4 @@
 /**
- * Representa uma coordenada do jogo puzzle-8.
- */
-class Coordenadas {
-
-    /**
-     * Constrói uma nova instância da classe Coordeadas.
-     * @param {Number} linha Linha em que o bloco está.
-     * @param {Number} coluna Coluna em que o bloco está
-     */
-    constructor(linha = null, coluna = null) {
-        this.__linha = linha;
-        this.__coluna = coluna;
-    }
-
-    /**
-     * Obtém o valor da linha do bloco.
-     */
-    get linha() {
-        return this.__linha;
-    }
-
-    /***
-     * Altera o valor da linha do bloco.
-     */
-    set linha(novaLinha) {
-        this.__linha = novaLinha;
-    }
-
-    /**
-     * Obtém o valor da coluna do bloco.
-     */
-    get coluna() {
-        return this.__coluna;
-    }
-
-    /**
-     * Altera o valor da coluna do bloco.
-     */
-    set coluna(novaColuna) {
-        this.__coluna = novaColuna;
-    }
-}
-
-/**
  * Objeto que marcará o tempo no jogo
  */
 let __timer;
